cff239ccb4e220822a560dd78f2ad90e17f09ccd
Stuart Myatt <myattstuart1976@gmail.com>
2026-08-28 10:49:40 +0100

    Bump version to 1.7 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "uk.co.tacklebox.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
     }
     signingConfigs {
         create("release") {
